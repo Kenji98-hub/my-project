0f9b9c764ec222c84f3cff79064f241dba3e45ac
Kenji98-hub <kennethmunion4@gmail.com>
2024-11-06 23:22:49 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 
 	<title>My first project</title>
 </head>
+
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 <body>
 
 <p>My Details </p>
@@ -25,5 +28,9 @@
      <imput type="text" name="lastname">  
     </div>
 
+   
+
 </body>
 </html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
