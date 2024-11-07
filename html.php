5b5ff235bcb58b7f8cf37c0e72cbd269439411dd
Kenji98-hub <kennethmunion4@gmail.com>
2024-11-06 23:57:45 -0800

    Add files via upload

---
+++ html.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>My first project</title>
+</head>
+
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<body>
+
+<p>My Stupid self</p>
+<br>
+   <div> 
+     <label>First name:</label>
+     <input type="text" name="firstname">  
+   </div>
+  <br>
+
+    <div> 
+     <label>Middle name:</label>
+     <input type="text" name="mname">  
+    </div>
+<br>
+   <div> 
+     <label>Last name:</label>
+     <input type="text" name="lastname">  
+   </div>
+<br>
+   <div>
+    <input type="submit" name="ngeek">
+   </div>
+
+   
+
+</body>
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
