44024a38f783061392073f51bea245a2325876e3
Kenji98-hub <kennethmunion4@gmail.com>
2024-11-06 23:08:59 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>My first project</title>
+</head>
+<body>
+
+<p>My Details </p>
+  
+   <div> 
+     <label>First name</label>
+     <imput type="text" name="firstname">  
+    </div>
+
+       <div> 
+     <label>Middle name</label>
+     <imput type="text" name="mname">  
+    </div>
+     
+   <div> 
+     <label>Last name</label>
+     <imput type="text" name="lastname">  
+    </div>
+
+</body>
+</html>
